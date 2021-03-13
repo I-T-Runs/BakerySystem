63047d801712f6552136527d3bac10136ea32d9c
Mthiz <kingzwide4@gmail.com>
2021-03-13 14:46:03 +0200

    Delete src directory

--- src/java/MrApplication.java
+++
@@ -1,9 +0,0 @@
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-/**
- *
- * @author Themba
- */
-@ApplicationPath("/app")
-public class MrApplication extends Application {}
