0cec3994b7f6f7b093fb88fd5d78a5e4907a440f
KeoagileKoitsioe <79352082+KeoagileKoitsioe@users.noreply.github.com>
2021-03-19 13:15:18 +0000

    ORDER DAO MOST RECENT

---
+++ BakerySystemRest/dao/OrderDao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bakerysystem.dao;
+
+import com.bakerysystem.Model.Order;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author keoagile
+ */
+public interface OrderDao {
+    boolean addOrder(Order order);
+    Order getOrder(int orderId);
+    ArrayList<Order> getAllOrders();
+    ArrayList<Order> getDeliveredOrders();
+    ArrayList<Order> OustandingOrders();
+    ArrayList<Order> PreparedOrders();
+    boolean updateOrderStatus(Order ord);
+    boolean removeOrder(int orderId);
+    ArrayList<Order> getOrdersByCustomer(int customerId);
+}
